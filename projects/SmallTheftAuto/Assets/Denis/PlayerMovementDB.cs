6297d707de671a02fefeb128c6b5911ea29ba226
deuimint <denisejoyy@icloud.com>
2021-10-18 21:17:16 +0200

    update PVdb

--- projects/SmallTheftAuto/Assets/Denis/PlayerMovementDB.cs
+++ projects/SmallTheftAuto/Assets/Denis/PlayerMovementDB.cs
@@ -16,23 +16,26 @@ public class PlayerMovementDB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         // Use Input.GetAxis() instead of Input.GetKey()
         float translation = Input.GetAxis("Vertical") * speed;
         float roation = Input.GetAxis("Horizontal") * rotation;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
         // translate the player on the y-axis (which points up)
-        transform.Translate(0f, 0.01f, 0f);
+        transform.Translate(0, translation, 0);
+        transform.Translate(0, 0, rotation);
 
         // First Exercise:
-    // make him walk backwards, when S is pressed
-        
-    // Second Exercise:
-    // make him turn left, if A is pressed
-    // transform.Rotate(xAngle, yAngle, zAngle);
-    // make him turn right, if D is pressed
-        
-    // Third Exercise:
-    // What problem exists again with Update, Movement, Frame-Rate (FPS)?
-        
-    // Fourth Exercise:
-    // Use Input.GetAxis() instead of Input.GetKey()
-        }
+        // make him walk backwards, when S is pressed
+
+        // Second Exercise:
+        // make him turn left, if A is pressed
+        // transform.Rotate(xAngle, yAngle, zAngle);
+        // make him turn right, if D is pressed
+
+        // Third Exercise:
+        // What problem exists again with Update, Movement, Frame-Rate (FPS)?
+    }
 }
