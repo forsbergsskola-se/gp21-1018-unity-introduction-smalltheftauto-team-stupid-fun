894f3b6bf6c27b4c0f4286db0ed813d9ab04bf66
deuimint <denisejoyy@icloud.com>
2021-10-18 20:36:48 +0200

    update PlayerMovDB script

--- projects/SmallTheftAuto/Assets/Denis/PlayerMovementDB.cs
+++ projects/SmallTheftAuto/Assets/Denis/PlayerMovementDB.cs
@@ -12,7 +12,25 @@ public class PlayerMovementDB : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+        // only, if the W-Key is currently pressed...
+        if (Input.GetKey(KeyCode.W)) {
+        // translate the player on the y-axis (which points up)
+        transform.Translate(0f, 0.01f, 0f);
+    }
         
+    // First Exercise:
+    // make him walk backwards, when S is pressed
+        
+    // Second Exercise:
+    // make him turn left, if A is pressed
+    // transform.Rotate(xAngle, yAngle, zAngle);
+    // make him turn right, if D is pressed
+        
+    // Third Exercise:
+    // What problem exists again with Update, Movement, Frame-Rate (FPS)?
+        
+    // Fourth Exercise:
+    // Use Input.GetAxis() instead of Input.GetKey()
+
     }
 }
