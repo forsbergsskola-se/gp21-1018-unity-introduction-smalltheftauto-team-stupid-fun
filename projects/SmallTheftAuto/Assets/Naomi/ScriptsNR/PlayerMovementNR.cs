b8c7c90f8e6a7294a543934fec6d217ba4a8dec6
GREM <naomiruokamo@gmail.com>
2021-10-19 09:22:57 +0200

    playermovementNR script

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/PlayerMovementNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/PlayerMovementNR.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovementNR : MonoBehaviour
 {
+    private float speed = 20f;
+    private float rotationspeed = -400f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,18 @@ public class PlayerMovementNR : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationspeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
         
+        transform.Translate(0f, translation, 0f);
+        transform.Rotate(0, 0, rotation);
+        
+
+
     }
 }
