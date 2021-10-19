1496752b145fc58d19fca3b114a31bc043b0ff90
GREM <naomiruokamo@gmail.com>
2021-10-19 16:56:47 +0200

    Update CarFunctionsNR.cs

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/CarFunctionsNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/CarFunctionsNR.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CarFunctionsNR : MonoBehaviour
 {
     
+    public PlayerMovementNR movement;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement.enabled = false;
     }
 
     // Update is called once per frame
