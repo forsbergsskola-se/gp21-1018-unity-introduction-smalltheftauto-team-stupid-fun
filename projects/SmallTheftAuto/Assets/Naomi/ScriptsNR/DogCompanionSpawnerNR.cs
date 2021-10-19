a840fd73b195cb7b58568d778483b10dcb729b36
GREM <naomiruokamo@gmail.com>
2021-10-19 13:48:45 +0200

    Fixed dog spawner problem NR

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DogCompanionSpawnerNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DogCompanionSpawnerNR.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DogCompanionSpawnerNR : MonoBehaviour
 {
+    public GameObject dogPrefab;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(dogPrefab);
     }
 
     // Update is called once per frame
