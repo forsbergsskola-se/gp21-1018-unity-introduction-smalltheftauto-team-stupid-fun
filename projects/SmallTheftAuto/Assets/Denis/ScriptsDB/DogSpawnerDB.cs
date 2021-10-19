fb7179b87eb9b4d9672f639bc45e9e3d4e69234d
deuimint <denisejoyy@icloud.com>
2021-10-19 21:41:08 +0200

    redid the entire thing

--- projects/SmallTheftAuto/Assets/Denis/ScriptsDB/DogSpawnerDB.cs
+++ projects/SmallTheftAuto/Assets/Denis/ScriptsDB/DogSpawnerDB.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoggySpawnerDB : MonoBehaviour
+public class DogSpawnerDB : MonoBehaviour
 {
-    private GameObject dogPrefab;
+    public GameObject dogPrefab;
     private GameObject dogInstance;
-    
-    // Start is called before the first frame update
+
+// Start is called before the first frame update
     void OnEnable()
     {
         dogInstance = Instantiate(dogPrefab);
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
-        
+    
     }
     void OnDisable()
     {
--- projects/SmallTheftAuto/Assets/Denis/ScriptsDB/playermovementDB.cs
+++ projects/SmallTheftAuto/Assets/Denis/ScriptsDB/playermovementDB.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementDB : MonoBehaviour
+public class playermovementDB : MonoBehaviour
 {
     private float speed = 10f;
     private float rotationspeed = -200f;
-    
-    // Start is called before the first frame update
+
+// Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void FixedUpdate()
     {
         // First Exercise: make him walk backwards, when S is pressed
         // Use Input.GetAxis() instead of Input.GetKey()
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationspeed;
-        
+    
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
-        
+    
         // translate the player on the y-axis (which points up)
         transform.Translate(0, translation, 0);
         transform.Rotate(0, 0, rotation);
@@ -35,6 +35,7 @@ public class PlayerMovementDB : MonoBehaviour
         // make him turn right, if D is pressed
 
         // Third Exercise:
-        // What problem exists again with Update, Movement, Frame-Rate (FPS)?
+        // What problem exists again with Update, Movement, Frame-Rate (FPS)?{
+        
     }
 }
