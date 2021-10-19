4f19bbf6c6f011d55c764a0c6cf3f9baaf4cbc3d
GREM <naomiruokamo@gmail.com>
2021-10-19 09:50:28 +0200

    fixes player location NR

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/PlayerMovementNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/PlayerMovementNR.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class PlayerMovementNR : MonoBehaviour
 {
-    private float speed = 20f;
-    private float rotationspeed = -400f;
-    
-    
+    float speed = 20f;
+    float rotationspeed = -400f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationspeed;
-
+        float rotation = Input.GetAxis("Horizontal")  * rotationspeed;
+        
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
-        
-        transform.Translate(0f, translation, 0f);
-        transform.Rotate(0, 0, rotation);
-        
 
+        transform.Translate(0f, translation,0f);
+        transform.Rotate(0, 0, rotation);
 
     }
 }
