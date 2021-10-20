3b1969a2382c880464a6ea16c19524afd9308b84
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-10-20 23:11:00 +0200

    Adds Zoo class + AddAnimal method

--- projects/ZooEK/Program.cs
+++ projects/ZooEK/Program.cs
@@ -1,32 +1,52 @@
 ﻿using System;
 
 namespace ZooEK {
-
     abstract class Program {
-
-        static void Main() {
+        static void Main()
+        {
 
         }
 
-        public void AddAnimal<TAnimals>() {
-            Animal animal = new Animal();
-        }
+        // public Animal AddAnimal<TAnimals>() {
+        //     Animal animal = new Animal();
+        //     if (typeof(TAnimals) is Animal) {
+        //         return animal;
+        //     } else {
+        //         return null;
+        //     }
+        // }
 
-        // public bool HasAnimal() {
-        //     if (Animal) {
+    }
+
+    class Zoo<TAnimals> where TAnimals : Animal {
+
+        //Animal[] zooAnimals = { };
+        private Animal[] animalZoo = new Animal[0];
+        public Zoo<Fish> fishZoo = new Zoo<Fish>();
+        public Zoo<Lion> lionZoo = new Zoo<Lion>();
+        public Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+        public Zoo<Mammal> mammalZoo = new Zoo<Mammal>();
+
+        //Zoo<Fish> fishZoo = new Zoo<Fish>();
+        //Console.WriteLine("This should be True: "+fishZoo.HasAnimal<Clownfish>());
+
+        // public bool HasAnimal<TAnimals>() {
+        //     if () {
         //         return true;
         //     } else {
         //         return false;
         //     }
         // }
-    }
 
-    class Zoo<TAnimals> where TAnimals : Animal {
-        protected string[] zooAnimals = {"Animal"};
+        public void AddAnimal(TAnimals animal) {
+            Array.Resize<Animal>(ref animalZoo, animalZoo.Length + 1);
+            animalZoo[animalZoo.Length - 1] = animal;
+        }
     }
 
     class Animal {
-        public override string ToString() => GetType().Name;
+        //string name;
+        //public override string ToString() => GetType().Name;
     }
 
     class Mammal : Animal { }
