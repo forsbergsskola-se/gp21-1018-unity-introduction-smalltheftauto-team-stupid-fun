c36fae71fc6aedf425202f8dd37a095edb45ab91
deuimint <denisejoyy@icloud.com>
2021-10-21 15:59:53 +0200

    car movement and camerafollow

--- projects/SmallTheftAuto/Assets/CameraFollowDB.cs
+++ projects/SmallTheftAuto/Assets/CameraFollowDB.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarMovementDB : PlayerMovement
+public partial class CameraFollowDB : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, 5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
-        transform.Rotate(0f,0f,-400 * Time.deltaTime * Input.GetAxis("Horizontal"));
+        
     }
 }
