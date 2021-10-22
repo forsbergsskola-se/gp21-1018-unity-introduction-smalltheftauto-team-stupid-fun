41a74ede141039027986e7317be379dad32f23bf
GREM <naomiruokamo@gmail.com>
2021-10-22 15:36:41 +0200

    Create CarMovementNR.cs

---
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/CarMovementNR.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementNR : MonoBehaviour
+{
+    float speed = 20f;
+    float rotationspeed = -200f;
+
+    
+    void Start()
+    {
+
+    }
+    
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal")  * rotationspeed;
+        
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, translation,0);
+        transform.Rotate(0, 0, rotation);
+    }
+}
