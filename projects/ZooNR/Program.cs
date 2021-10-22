1b91d5e23d4f61d35b5cf403d01f33c21b547136
GREM <naomiruokamo@gmail.com>
2021-10-22 11:22:01 +0200

    creates my zoo folder

---
+++ projects/ZooNR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZooNR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
