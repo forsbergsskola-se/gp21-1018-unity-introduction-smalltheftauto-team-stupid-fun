65b61331e60c058593366c2d7746b717af83b6fa
GREM <naomiruokamo@gmail.com>
2021-11-02 17:25:20 +0100

    Update TimerNR.cs

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/TimerNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/TimerNR.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerNR : MonoBehaviour
 {
+    private float timePassed;
+    public Text timeText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class TimerNR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.deltaTime;
+        DisplayTime(timePassed);
     }
+
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    
 }
