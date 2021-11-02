18dc974828ac44e18420e424034b0eeac344fa5d
GREM <naomiruokamo@gmail.com>
2021-11-02 15:42:56 +0100

    starting on the DriverNR script

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DriverNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DriverNR.cs
@@ -13,6 +13,10 @@ public class DriverNR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Interact-Vehicle"))
+        {
+            FindObjectOfType<VechileNR>();
+            
+        }
     }
 }
