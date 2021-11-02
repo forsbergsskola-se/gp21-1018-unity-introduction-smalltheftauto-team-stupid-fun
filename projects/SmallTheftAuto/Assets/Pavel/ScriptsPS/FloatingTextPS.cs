5057483dd487cab89555d538b48270d635835f52
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-02 13:40:42 +0100

    Added Colors to material, also improving car movement

--- projects/SmallTheftAuto/Assets/Pavel/ScriptsPS/FloatingTextPS.cs
+++ projects/SmallTheftAuto/Assets/Pavel/ScriptsPS/FloatingTextPS.cs
@@ -5,23 +5,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-[RequireComponent(typeof(TMP_Text))]
 public class FloatingTextPS : MonoBehaviour
 {
-
-    public string displayText;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         
-
-        if (GetComponent<CarMovementPS>() == enabled )
-        {
-            TMP_Text tmp_text = GetComponent<TMP_Text>();
-            tmp_text.text = displayText;
-            tmp_text.D
-        }
+        
     }
 
     // Update is called once per frame
