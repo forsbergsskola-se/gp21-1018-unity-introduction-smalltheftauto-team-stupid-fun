5153afada83ca47a5567fd2d4e98f5c876ba70f8
GREM <naomiruokamo@gmail.com>
2021-11-02 11:18:42 +0100

    added Zoo Classes

--- projects/ZooNR/Program.cs
+++ projects/ZooNR/Program.cs
@@ -9,4 +9,50 @@ namespace ZooNR
             Console.WriteLine("Hello World!");
         }
     }
+
+
+    class Student
+    {
+        
+    }
+    class Animal
+    {
+        
+    }
+    
+    class Fish : Animal
+    {
+        
+    }
+
+    class Mammal : Animal
+    {
+        
+    }
+
+    class Bear : Mammal
+    {
+        
+    }
+    
+    class Donkey : Mammal
+    {
+        
+    }
+    
+    class Lion : Mammal
+    {
+        
+    }
+    
+    class Salmon : Fish
+    {
+        
+    }
+    
+    class Clownfish : Fish
+    {
+        
+    }
+    
 }
