0333b64019cde10a582c8b5592866353e39fd18e
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-02 12:06:54 +0100

    Trying to get the floating text going

--- projects/SmallTheftAuto/Assets/Pavel/ScriptsPS/FloatingTextPS.cs
+++ projects/SmallTheftAuto/Assets/Pavel/ScriptsPS/FloatingTextPS.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+[RequireComponent(typeof(TMP_Text))]
 public class FloatingTextPS : MonoBehaviour
 {
-    public GameObject textPrefab;
-    private GameObject textInstance;
+
+    public string displayText;
     
     // Start is called before the first frame update
-    void OnEnable()
+    void Start()
     {
-        textInstance = Instantiate(textPrefab);
+        
+
+        if (GetComponent<CarMovementPS>() == enabled )
+        {
+            TMP_Text tmp_text = GetComponent<TMP_Text>();
+            tmp_text.text = displayText;
+            tmp_text.D
+        }
     }
 
     // Update is called once per frame
-    void OnDisable()
-    {
-        Destroy(textInstance);
-    }
+    
 }
