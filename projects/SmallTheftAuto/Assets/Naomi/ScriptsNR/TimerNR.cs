0caeef3b29fe9ae797ccef878c98a37e870b8cb9
GREM <naomiruokamo@gmail.com>
2021-11-02 17:47:30 +0100

    Finished TimerNR script
    restarts the scene and counts down from 2 min

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/TimerNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/TimerNR.cs
@@ -1,26 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TimerNR : MonoBehaviour
 {
-    private float timePassed;
-    public Text timeText;
+    public float timeRemaining = 120;
+    public bool timerIsRunning = false;
+    public TMPro.TextMeshProUGUI timeText;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        timerIsRunning = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        timePassed += Time.deltaTime;
-        DisplayTime(timePassed);
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene("SceneN");
+            }
+        }
     }
-
 
     void DisplayTime(float timeToDisplay)
     {
@@ -29,7 +41,6 @@ public class TimerNR : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
-        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeText.text = string.Format("{0} min {1:00}s", minutes, seconds);
     }
-    
 }
