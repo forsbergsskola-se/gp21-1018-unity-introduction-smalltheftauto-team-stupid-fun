dfc0da552cefad07d245a657f123f1ea014cc52b
deuimint <denisejoyy@icloud.com>
2021-11-03 20:31:37 +0100

    playermov and phonebox quest

---
+++ projects/SmallTheftAuto/Assets/Denis/ScriptsDB/PhoneBoxQuestDB.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneBoxQuestDB : PlayerMovement
+{
+    public GameObject Interaction;
+    private void Start()
+    {
+        Interaction.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- projects/SmallTheftAuto/Assets/Denis/ScriptsDB/playermovementDB.cs
+++ projects/SmallTheftAuto/Assets/Denis/ScriptsDB/playermovementDB.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class playermovementDB : MonoBehaviour
 {
-    private float speed = 10f;
-    private float rotationspeed = -200f;
 
 // Start is called before the first frame update
     void Start()
@@ -14,28 +12,9 @@ public class playermovementDB : MonoBehaviour
     }
 
 // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // First Exercise: make him walk backwards, when S is pressed
-        // Use Input.GetAxis() instead of Input.GetKey()
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationspeed;
-    
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-    
-        // translate the player on the y-axis (which points up)
-        transform.Translate(0, translation, 0);
-        transform.Rotate(0, 0, rotation);
-
-
-        // Second Exercise:
-        // make him turn left, if A is pressed
-        // transform.Rotate(xAngle, yAngle, zAngle);
-        // make him turn right, if D is pressed
-
-        // Third Exercise:
-        // What problem exists again with Update, Movement, Frame-Rate (FPS)?{
-        
+        transform.Translate(0f, 5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
+        transform.Rotate(0f,0f,-180 * Time.deltaTime * Input.GetAxis("Horizontal"));
     }
 }
