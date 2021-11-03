174b91017ad9e660998ff830c4a72d48cd8d6bb5
GREM <naomiruokamo@gmail.com>
2021-11-03 18:18:05 +0100

    Finished Health Bar

---
+++ projects/SmallTheftAuto/Assets/Naomi/UI Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarNR : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        
+        // puts the health bar gradient to green
+        fill.color = gradient.Evaluate(1f);
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        
+        // changes the health bar gradient to whatever the health is
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
