99a37511d006448802092d732a7e9eb80a8a8239
GREM <naomiruokamo@gmail.com>
2021-11-03 14:41:08 +0100

    Creates Parking Spot Script NR

---
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/ParkingSpotNR.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingSpotNR : MonoBehaviour
+{
+
+    public bool hasCar;
+    public GameObject carPrefabNR;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hasCar)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
