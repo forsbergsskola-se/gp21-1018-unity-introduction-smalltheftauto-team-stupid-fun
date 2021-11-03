b45c16120b770d9a47bd7de474855de251518fd6
GREM <naomiruokamo@gmail.com>
2021-11-03 15:30:20 +0100

    FloatingTextNR colors and fade

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/FloatingTextNR.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/FloatingTextNR.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FloatingTextNR : MonoBehaviour
 {
+    private TMPro.TextMeshProUGUI text;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = FindObjectOfType<TMPro.TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (text.color.a > 0)
+        {
+            this.transform.position += new Vector3(0, 2, 0) * Time.deltaTime;
+            Color color = text.color;
+            color.a -= 0.01f;
+            text.color = color;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
