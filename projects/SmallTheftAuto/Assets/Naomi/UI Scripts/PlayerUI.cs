f66fcaeba6e14f8eae86d75c3a9ce7a91db3eeeb
GREM <naomiruokamo@gmail.com>
2021-11-04 11:00:24 +0100

    Update PlayerUI.cs

--- projects/SmallTheftAuto/Assets/Naomi/UI Scripts/PlayerUI.cs
+++ projects/SmallTheftAuto/Assets/Naomi/UI Scripts/PlayerUI.cs
@@ -11,14 +11,14 @@ public class PlayerUI : MonoBehaviour
     public int currentHealth;
     public HealthBar healthBar;
     
-    // Start is called before the first frame update
+
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (currentHealth <= 0)
@@ -41,6 +41,4 @@ public class PlayerUI : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth, 0, 100);
         healthBar.SetHealth(currentHealth);
     }
-
-
 }
