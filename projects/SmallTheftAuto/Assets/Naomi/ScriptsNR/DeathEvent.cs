2942dfc0c886ac18a46aecb176c19c94d25a02a7
GREM <naomiruokamo@gmail.com>
2021-11-04 15:56:36 +0100

    Update DeathEvent.cs

--- projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DeathEvent.cs
+++ projects/SmallTheftAuto/Assets/Naomi/ScriptsNR/DeathEvent.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class DeathEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private GameObject player;
+    private TMPro.TextMeshProUGUI deathText;
+    public float respawnTime;
+    public bool isDead;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
+    {
+   
+    }
+
+    public void PlayerDeathEvent()
+    {
+        isDead = true;
+        Invoke("Respawn", respawnTime);
+    }
+
+    public void Respawn()
+    {
+        playerPosition.transform.position = respawnPoint.transform.position;
+        //Physics.SyncTransforms(); this is if we have a trigger to cause the death then uncomment this
+        //player.GetComponent<Currency>().LoseMoney();
+        player.GetComponent<HealthSystem>().ResetHealth();
+        isDead = false;
+    }
+
+    public void DeathText()
     {
         
     }
+
+
 }
