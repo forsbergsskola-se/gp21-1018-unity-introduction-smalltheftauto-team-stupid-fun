320d13bc3fee6ffdc1a54d6c4741c463616c6d86
GREM <naomiruokamo@gmail.com>
2021-11-05 12:41:58 +0100

    Update WeaponUI.cs

--- projects/SmallTheftAuto/Assets/Naomi/UI Scripts/WeaponUI.cs
+++ projects/SmallTheftAuto/Assets/Naomi/UI Scripts/WeaponUI.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Sprite fist;
+    public Sprite pistol;
+    public Sprite machinegun;
+    
+    
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            this.gameObject.GetComponent<Image>().sprite = fist;
+        }
         
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            this.gameObject.GetComponent<Image>().sprite = pistol;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            
+        {
+            this.gameObject.GetComponent<Image>().sprite = machinegun;
+        }
     }
 }
