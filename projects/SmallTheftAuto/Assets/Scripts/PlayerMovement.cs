19673f31d8cd619570a466fa946b7768c0a239b9
GREM <naomiruokamo@gmail.com>
2021-11-05 14:41:41 +0100

    putting in player movement

--- projects/SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+    
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector3 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y ,lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
