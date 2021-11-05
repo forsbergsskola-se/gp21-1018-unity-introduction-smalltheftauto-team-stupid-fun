415ab1f25cb9b38c404458d53e778a1d8f5a0b53
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-06 00:04:43 +0100

    Follow Car with cam

--- projects/SmallTheftAuto/Assets/Scripts/VehicleEK.cs
+++ projects/SmallTheftAuto/Assets/Scripts/VehicleEK.cs
@@ -6,6 +6,7 @@ public class VehicleEK : MonoBehaviour {
 
     private DriverEK driver;
     public GameObject car;
+    public GameObject mainCamera;
 
     void Update() {
         if (Input.GetButtonDown("Interact-Vehicle")) {
@@ -20,6 +21,7 @@ public class VehicleEK : MonoBehaviour {
         car.GetComponent<DriverEK>();
         driver.gameObject.SetActive(false);
         GetComponent<CarControllerEK>().enabled = true;
+        mainCamera.gameObject.SetActive(false);
     }
 
     void ExitCar() {
@@ -27,5 +29,6 @@ public class VehicleEK : MonoBehaviour {
         driver.gameObject.SetActive(true);
         driver = null;
         GetComponent<CarControllerEK>().enabled = false;
+        mainCamera.gameObject.SetActive(true);
     }
 }
